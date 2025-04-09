c430e3b4f53c27e48a740ea4ad186869ccbd5740
Jade Browne <jade.browne@outlook.com>
2025-04-09 15:16:43 -0400

    route error

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,10 +2,6 @@ const router = require("express").Router();
 const controllers = require("../controllers");
 const checkAuth = require("../middleware/auth");
 
-router.get("/", ({ session: { isLoggedIn } }, res) => {
-  res.render("index", { isLoggedIn });
-});
-
 router.get("/login", async (req, res) => {
   if (req.session.isLoggedIn) return res.redirect("/");
   res.render("login", { error: req.query.error });
